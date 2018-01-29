a0d05e5834bdc1cddc4411cec86e57a3f389a5d1
abhishekkhasgiwala <abhishek.khasgiwala@gmail.com>
2018-01-29 23:06:30 +0000

    Add files via upload
    Initial commit as part of dummy test site for web crawler application

---
+++ TestSite/src/main/java/com/ak/crawler/TestSite/TestSiteApplication.java
@@ -0,0 +1,12 @@
+package com.ak.crawler.TestSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSiteApplication.class, args);
+	}
+}
---
+++ TestSite/src/test/java/com/ak/crawler/TestSite/TestSiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ak.crawler.TestSite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestSiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
